1e16f381e28aa5e1bd43cc48327422a09aba256b
shaam.sharan <111180217+ShaamSharan@users.noreply.github.com>
2022-08-13 04:12:59 -0800

    Update Steamlit_app.py

--- Steamlit_app.py
+++ Steamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣 🐔 🍞Hi my 1st Python program Text1')
 streamlit.text('🥗Hi my 1st Python program Text2')
 streamlit.text('🥑Hi my 1st Python program Text3')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
