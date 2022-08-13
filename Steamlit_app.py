bf346660da3a26c62eca6b86687b8ff499229990
shaam.sharan <111180217+ShaamSharan@users.noreply.github.com>
2022-08-13 08:57:25 -0800

    Update Steamlit_app.py

--- Steamlit_app.py
+++ Steamlit_app.py
@@ -1,7 +1,14 @@
-
+import streamlit
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
 
+streamlit.title('Hi my 1st Python program')
+streamlit.header(' Hi my 1st Python program Header')
+
+streamlit.text('🥣 🐔 🍞Hi my 1st Python program Text1')
+
+streamlit.text('🥗Hi my 1st Python program Text2')
+streamlit.text('🥑Hi my 1st Python program Text3')
