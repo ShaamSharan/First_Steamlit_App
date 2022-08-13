5aa0e273197b3feb57f5290a5fa9a5d3607052fb
shaam.sharan <111180217+ShaamSharan@users.noreply.github.com>
2022-08-13 03:15:44 -0800

    Update Steamlit_app.py

--- Steamlit_app.py
+++ Steamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit
 streamlit.title('Hi my 1st Python program')
+streamlit.header('Hi my 1st Python program Header')
+
+streamlit.text('Hi my 1st Python program Text1')
+
+streamlit.text('Hi my 1st Python program Text2')
+streamlit.text('Hi my 1st Python program Text3')
+
