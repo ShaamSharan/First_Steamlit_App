52c7154417954267cc4f2533d62577d8f507d08b
shaam.sharan <111180217+ShaamSharan@users.noreply.github.com>
2022-08-24 09:25:06 -0800

    Update Steamlit_app.py

--- Steamlit_app.py
+++ Steamlit_app.py
@@ -22,7 +22,7 @@ import requests
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # streamlit.text(fruityvice_response.json())
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
 fruityvice.normalize=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe (fruityvice.normalize)
 
