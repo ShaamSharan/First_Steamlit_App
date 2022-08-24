c289db64ac41869b6efca5517d0ab6c28c7b9675
shaam.sharan <111180217+ShaamSharan@users.noreply.github.com>
2022-08-24 09:24:00 -0800

    Update Steamlit_app.py

--- Steamlit_app.py
+++ Steamlit_app.py
@@ -19,8 +19,13 @@ streamlit.text('🥑Hi my 1st Python program Text3')
 
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# streamlit.text(fruityvice_response.json())
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruityvice.normalize=pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe (fruityvice.normalize)
+
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
